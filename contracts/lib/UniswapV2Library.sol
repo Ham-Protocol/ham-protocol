cecd57b86fff115d956112932bb379afc5671ac7
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-09-02 13:21:27 -0400

    Fix library

--- contracts/lib/UniswapV2Library.sol
+++ contracts/lib/UniswapV2Library.sol
@@ -27,7 +27,7 @@ library UniswapV2Library {
     // fetches and sorts the reserves for a pair
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
-        (uint reserve0, uint reserve1,) = IUniswapV2Pair(pairFor(factory, tokenA, tokenB)).getReserves();
+        (uint reserve0, uint reserve1,) = UniswapPair(pairFor(factory, tokenA, tokenB)).getReserves();
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
